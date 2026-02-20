5d7ec6064fc86e541c894cbe4b21607a7aa1f95d
FunkyFight <alexisdeschampsgamer@gmail.com>
2026-02-20 16:57:32 +0100

    removed condition in create cords

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Données/Documents/stupid C projects/labyrinthe
-# Build directory: D:/Données/Documents/stupid C projects/labyrinthe/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[labyrinthe_smoke]=] "D:/Données/Documents/stupid C projects/labyrinthe/cmake-build-debug/labyrinthe_smoke.exe")
-set_tests_properties([=[labyrinthe_smoke]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Données/Documents/stupid C projects/labyrinthe/CMakeLists.txt;81;add_test;D:/Données/Documents/stupid C projects/labyrinthe/CMakeLists.txt;0;")
---
+++ events/step_by_step_event.h
@@ -0,0 +1,13 @@
+//
+// Created by Funky on 19/02/2026.
+//
+
+#ifndef LABYRINTHE_STEP_BY_STEP_EVENT_H
+#define LABYRINTHE_STEP_BY_STEP_EVENT_H
+
+#include "../types.h"
+
+void event_on_space_key(struct Game *g);
+bool apply_next_step(struct Game *g);
+
+#endif //LABYRINTHE_STEP_BY_STEP_EVENT_H
